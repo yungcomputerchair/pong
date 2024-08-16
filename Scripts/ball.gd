a161458cbf3d691c02bf7784fa976096d47c3e63
Gent Semaj <me@ycc.dev>
2024-08-15 22:18:47 -0700

    Make ball collider circular + only rotate sprite

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
+var sprite;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	sprite = find_child("Sprite2D")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate(deg_to_rad(30) * delta)
+	sprite.rotate(deg_to_rad(30) * delta)
