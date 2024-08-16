d3f841b465dbb7e6492edd0c36e61148a3a5f748
Gent Semaj <me@ycc.dev>
2024-08-16 15:29:33 -0700

    Winner label

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -3,6 +3,14 @@ extends Control
 @onready var left_panel = %LeftPanel
 @onready var right_panel = %RightPanel
 
+func update_panel(panel, score):
+	panel.get_node("Score").text = str(score)
+	var status = panel.get_node("Status")
+	if score >= 10:
+		status.text = "WINNER"
+	else:
+		status.text = ""
+
 func update_scores(left, right):
-	left_panel.get_node("Label").text = str(left)
-	right_panel.get_node("Label").text = str(right)
+	update_panel(left_panel, left)
+	update_panel(right_panel, right)
